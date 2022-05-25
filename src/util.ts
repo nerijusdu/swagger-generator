30cc85d95cb3f5a6046a139226b7a92e71228b63
Nerijus <dnerijus72@gmail.com>
2022-05-25 23:17:41 +0300

    update name sanitization

--- src/util.ts
+++ src/util.ts
@@ -11,5 +11,9 @@ export const sanitizeRouteArgument = (route: ts.Expression | undefined, file: ts
 export const sanitizeTypeName = (name: string) => name
   .replaceAll(' ', '')
   .replace(/(Pick|Omit)<(.+),(.+)>/, (_, type, base, args) => `${type}_${base}_${md5(args)}`)
+  .replaceAll('|', '_')
+  .replaceAll('"', '')
+  .replaceAll('\\\'', '')
+  .replaceAll(',', '_')
   .replaceAll('<', '_')
   .replaceAll('>', '_');
